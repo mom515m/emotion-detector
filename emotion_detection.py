27472445d09d74f0ec344c69a2136a3f05a4ed77
mom515m <muzafferalvani11@gmail.com>
2026-07-21 11:14:18 +0300

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,7 +1,10 @@
+import json
 import requests
 
 
 def emotion_detector(text_to_analyze):
+    """Analyze the emotions expressed in the provided text."""
+
     url = (
         "https://sn-watson-emotion.labs.skills.network/"
         "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -25,4 +28,22 @@ def emotion_detector(text_to_analyze):
         timeout=30
     )
 
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotions["anger"]
+    disgust_score = emotions["disgust"]
+    fear_score = emotions["fear"]
+    joy_score = emotions["joy"]
+    sadness_score = emotions["sadness"]
+
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion
+    }
